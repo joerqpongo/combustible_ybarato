d5b88acd7aebfb2020d7cadbe9eb43aebd2b7c0c
Jose Luis Romero <tu_email@gmail.com>
2026-06-15 16:31:36 +0200

    Anadido script de inicio facil .bat

---
+++ Iniciar_App.bat
@@ -0,0 +1,9 @@
+@echo off
+title Iniciando Combustible y Barato
+echo ===================================================
+echo   Iniciando combustible_ybarato.es...
+echo   Por favor, espera a que se abra el navegador.
+echo   Para cerrar la aplicacion, cierra esta ventana.
+echo ===================================================
+python -m streamlit run app.py
+pause
